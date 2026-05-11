5373fa8fe52d85e0178fd1a599b7a677c80394f7
Aluno.Etec <aula@etec.com>
2026-05-11 16:36:05 -0300

    Parei aqui

--- gameroom/App.js
+++ gameroom/App.js
@@ -13,10 +13,42 @@ export default function App() {
   const [Id, useId] = useState(null);
 
   useEffect(() => {
-    //ele dispara informações com base do usuário.
+    socket.on('player', (id) => {
+      setImmediate(id);
+    });//ele dispara informações com base do usuário.
+
+    socket.on('update', (users) => {
+      setUsuarios(users);
+    });
+
+    return() => {
+      socket.off('player');
+      socket.off('update');
+    }
 
   }, []);
 
+  if (entrou == false) {
+    return (
+      <View>
+          <Text> Digite seu Nick:</Text>
+          <TextInput>
+            value={nome}
+            onChangeText={(novoTexto) => setNome(novoTexto)}
+          </TextInput>
+          <TouchableOpacity>
+            <Text>Entrar</Text>
+          </TouchableOpacity>
+      </View>
+    );
+  }
+else {
+  return (
+    <View>
+
+    </View>
+  );
+}
   return (
     <View>
     </View>
